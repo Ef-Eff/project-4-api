3209f11089817762b55f11b011f09be042dc0c5d
Fabricio <ravaneliferreira@hotmail.co.uk>
2017-04-24 09:16:51 +0100

    heroku commit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,15 @@
 class TweetsController < ApplicationController
   skip_before_action :authenticate_user!
   def tweets
-    tweets = HTTParty.get("https://api.twitter.com/1.1/search/tweets.json?q=#{tweet_params}&result_type=popular", {
+    tweets = HTTParty.get("https://api.twitter.com/1.1/search/tweets.json?q=#{tweet_params[:trend_title]}&result_type=recent", {
       headers: {
         'Content-Type' => 'application/json',
         'Authorization' => ENV["TWITTER_AUTHORIZATION"]
       }
     })
 
-    bob = tweets["statuses"].map {|element| element["text"] }
 
-    render json: bob
+    render json: tweets
   end
 
   def tweet_params
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -15,7 +15,7 @@ class VotesController < ApplicationController
 
   # POST /votes
   def create
-    vote_params[:value] = 1
+    
     @vote = Vote.new(vote_params)
     @vote.voter = current_user
 
